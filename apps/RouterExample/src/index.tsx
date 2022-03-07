72c1c5a6b6d5f3d45d25bba2b96e1f04ca807db3
Djobbo Maiga <djobbomaiga@LAP-MAC-0729.local>
2022-03-07 23:22:24 +0100

    add router #5

--- apps/RouterExample/src/index.tsx
+++ apps/RouterExample/src/index.tsx
@@ -1,18 +1,22 @@
 import { config as loadEnv } from "dotenv"
 import { client, renderMessage } from "./setupApp"
-import { App } from './App'
+import { App } from "./App"
+import { DiscordRouter } from '@reaccord/router'
 
 loadEnv()
 
 client.on("ready", () => console.log("Bot Started!"))
 
 client.on("messageCreate", (message) => {
-    
     const { content, channel } = message
 
-    if (content !== 'ram') return;
-    
-    renderMessage(channel, () => <App />)
+    if (content !== "router") return
+
+    renderMessage(channel, () => (
+        <DiscordRouter>
+            <App />
+        </DiscordRouter>
+    ))
 })
 
 client.login(process.env.DISCORD_TOKEN)
