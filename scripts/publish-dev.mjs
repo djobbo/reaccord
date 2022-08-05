cda3c16e9863143aa2d153c200819ebc13698e55
Djobbo <hi@djobbo.com>
2022-08-05 21:26:38 +0200

    upgrade publish:dev script

--- scripts/publish-dev.mjs
+++ scripts/publish-dev.mjs
@@ -1,4 +1,11 @@
 #!/usr/bin/env zx
+import stripAnsi from "strip-ansi"
+
+$.verbose = false
+
+const [, , ...args] = process.argv
+
+const dryRun = args.includes("--dry")
 
 const PLACEHOLDER_VERSION = "0.0.0-dev"
 
@@ -10,63 +17,134 @@ const packages = [
 	["canvas", "@reaccord/canvas"],
 ]
 
-const rawDevVersion = `${await $`npm show . version`}`.slice(0, -1)
+const logInfo = (...args) => console.log(chalk.gray("[Info]"), ...args)
+const logSuccess = (...args) => console.log(chalk.green("[Success]"), ...args)
+const logError = (...args) => console.log(chalk.red("[Error]"), ...args)
+const logWarning = (...args) => console.log(chalk.yellow("[Warning]"), ...args)
+const logBoxed = (msg) => {
+	const len = stripAnsi(msg).length + 2
+	console.log("╭" + "─".repeat(len) + "╮")
+	console.log("│ " + msg + " │")
+	console.log("╰" + "─".repeat(len) + "╯")
+}
+const newLine = () => console.log()
+
+const { version } = await fs.readJson("./package.json")
+logBoxed(`Publishing Reaccord ${chalk.green(`v${version}-dev`)}`)
 const gitShortHash = `${await $`git rev-parse --short HEAD`}`.slice(0, -1)
 
-console.log(`Checking latest dev version of '${mainPackage}'...`)
+logInfo(`Checking latest dev version of ${chalk.blue(mainPackage)}...`)
 const latestPackageVersion =
 	`${await $`pnpm view ${mainPackage}@dev version`}`.slice(0, -1)
+logInfo(
+	`Latest dev version of ${chalk.blue(mainPackage)} is ${chalk.green(
+		latestPackageVersion,
+	)}`,
+)
 
 const latestCommitPublished = latestPackageVersion.split(".").at(-1)
 if (latestCommitPublished === gitShortHash) {
-	console.log(`No new version found.`)
+	logWarning(`No new version found, skipping publish.`)
 	process.exit(0)
 }
 
 const timestamp = Date.now()
-const newDevVersion = `${rawDevVersion}.${timestamp}.${gitShortHash}`
+const newDevVersion = `${version}.${timestamp}.${gitShortHash}`
 
-console.log(`Publishing new dev version '${newDevVersion}'...`)
+newLine()
+logBoxed(`New dev version: ${chalk.green(`v${newDevVersion}`)}`)
 
+logInfo("Installing dependencies...")
 await $`pnpm ci`
+logSuccess("Dependencies installed.")
+newLine()
 
 for (const [packageFolder, packageName] of packages) {
-	console.log(`Updating ${packageName} version to ${newDevVersion}`)
+	logBoxed(`Publishing ${chalk.blue(packageName)}`)
 
 	// Deprecate the old version
+	logInfo("Checking for existing dev version...")
 	const oldDevVersion =
-		`${await $`pnpm view ${packageName}@dev version`}`.slice(0, -1)
+		`${await $`pnpm view ${packageName}@dev version || false`
+			// Will throw if package doesn't exist on the registry
+			.catch(() => "")}`.slice(0, -1)
 
 	if (!!oldDevVersion) {
+		logInfo(
+			`Existing dev version found, deprecating old dev version ${chalk.yellow(
+				oldDevVersion,
+			)}...`,
+		)
 		try {
-			await $`pnpm deprecate ${packageName}@${oldDevVersion} "no longer supported"`
-			console.log(`Deprecated '${packageName}@${oldDevVersion}'`)
+			if (!dryRun)
+				await $`pnpm deprecate ${packageName}@${oldDevVersion} "no longer supported"`
+
+			logSuccess(
+				`Deprecated ${chalk.yellow(`${packageName}@${oldDevVersion}`)}`,
+			)
 		} catch {
-			console.error(
-				`Failed to deprecate '${packageName}@${oldDevVersion}'`,
+			logError(
+				`Failed to deprecate ${chalk.yellow(
+					`${packageName}@${oldDevVersion}`,
+				)}`,
 			)
 		}
+	} else {
+		logInfo(`No existing dev version found, skipping deprecation.`)
 	}
+	newLine()
 
 	const packagePath = `packages/${packageFolder}`
 
 	await cd(packagePath)
 
 	// Build package
-	await $`pnpm build || true`
+	logInfo(`Building ${chalk.blue(packageName)}...`)
+	await $`pnpm build`
+	logSuccess(`Built ${chalk.blue(packageName)}`)
+	newLine()
 
 	// Update package version
-	await $`sed -i "s/workspace:${PLACEHOLDER_VERSION}/${newDevVersion}/g" package.json`
-	await $`sed -i "s/${PLACEHOLDER_VERSION}/${newDevVersion}/g" package.json`
+	logInfo(
+		`Updating ${chalk.blue(packageName)} version to ${chalk.green(
+			`v${newDevVersion}`,
+		)}`,
+	)
+	if (!dryRun) {
+		await $`sed -i "s/workspace:${PLACEHOLDER_VERSION}/${newDevVersion}/g" package.json`
+		await $`sed -i "s/${PLACEHOLDER_VERSION}/${newDevVersion}/g" package.json`
+	}
+	logSuccess(
+		`Updated ${chalk.blue(packageName)} version to ${chalk.green(
+			`v${newDevVersion}`,
+		)}`,
+	)
 
 	// Publish package
-	console.log(`Publishing '${packageName}'...`)
+	logInfo(`Publishing ${chalk.blue(packageName)} to NPM...`)
 	try {
-		await $`pnpm publish --no-git-checks --tag dev --access public`
-		console.log(`Published '${packageName}'@${newDevVersion}`)
+		if (!dryRun)
+			await $`pnpm publish --no-git-checks --tag dev --access public`
+
+		logSuccess(
+			`Published ${chalk.green(`${packageName}@${newDevVersion}`)}`,
+		)
+		logInfo(
+			`View package at ${chalk.gray(
+				`https://npmjs.com/package/${packageName}`,
+			)}`,
+		)
 	} catch {
-		console.error(`Failed to publish '${packageName}'@${newDevVersion}`)
+		logError(
+			`Failed to publish ${chalk.yellow(
+				`${packageName}@${newDevVersion}`,
+			)}`,
+		)
 	}
+	newLine()
 
 	await cd("../..")
 }
+
+logInfo(`${packages.length}/${packages.length} packages published.`)
+logBoxed(`Successfully published Reaccord ${chalk.green(`v${newDevVersion}`)}`)
