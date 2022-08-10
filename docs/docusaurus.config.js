960cb88c86c999d04859b910d636afd1d12235ae
Djobbo <hi@djobbo.com>
2022-08-10 19:17:16 +0200

    format docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,92 +6,91 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Reaccord",
-    tagline:
-        "A simple, and clean framework to build discord apps declaratively using React + JSX and Discord.js.",
-    url: "https://djobbo.github.io",
-    baseUrl: "/reaccord/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.ico",
+  title: "Reaccord",
+  tagline:
+    "A simple, and clean framework to build discord apps declaratively using React + JSX and Discord.js.",
+  url: "https://djobbo.github.io",
+  baseUrl: "/reaccord/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
-    organizationName: "djobbo",
-    projectName: "reaccord",
+  organizationName: "djobbo",
+  projectName: "reaccord",
 
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en"],
-    },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-    presets: [
-        [
-            "classic",
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    editUrl:
-                        "https://github.com/djobbo/reaccord/tree/master/docs",
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            }),
-        ],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/djobbo/reaccord/tree/master/docs",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
     ],
+  ],
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: "Reaccord",
-                logo: {
-                    alt: "Reaccord Logo",
-                    src: "img/reaccord.svg",
-                },
-                items: [
-                    {
-                        type: "doc",
-                        docId: "intro",
-                        position: "left",
-                        label: "Tutorial",
-                    },
-                    {
-                        href: "https://github.com/djobbo/reaccord",
-                        label: "GitHub",
-                        position: "right",
-                    },
-                ],
-            },
-            footer: {
-                style: "dark",
-                links: [
-                    {
-                        title: "Docs",
-                        items: [
-                            {
-                                label: "Tutorial",
-                                to: "/docs/intro",
-                            },
-                        ],
-                    },
-                    {
-                        title: "More",
-                        items: [
-                            {
-                                label: "GitHub",
-                                href: "https://github.com/djobbo/reaccord",
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} Djobbo Maiga and contributors. Built with Docusaurus.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Reaccord",
+        logo: {
+          alt: "Reaccord Logo",
+          src: "img/reaccord.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            href: "https://github.com/djobbo/reaccord",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/djobbo/reaccord",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Djobbo Maiga and contributors. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 }
 
 module.exports = config
