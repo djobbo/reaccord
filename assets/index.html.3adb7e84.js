6327572806f7a8c8dfa254b55ad122dae2e4fbf2
djobbo <4998293+djobbo@users.noreply.github.com>
2022-11-29 11:48:47 +0000

    Deploying to gh-pages from @ djobbo/reaccord@be6590679cad16b943595259fb3465902210d7fd 🚀

---
+++ assets/index.html.3adb7e84.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./app.872d8008.js";const n={};function _(o,r){return c(),t("div")}const a=e(n,[["render",_],["__file","index.html.vue"]]);export{a as default};
---
+++ assets/typescript.html.61dad143.js
@@ -0,0 +1,12 @@
+import{_ as s,o as n,c as a,e}from"./app.872d8008.js";const t={},o=e(`<h1 id="using-typescript" tabindex="-1"><a class="header-anchor" href="#using-typescript" aria-hidden="true">#</a> Using Typescript</h1><h2 id="installation" tabindex="-1"><a class="header-anchor" href="#installation" aria-hidden="true">#</a> Installation</h2><p>Reaccord has typescript support built-in, but for the optimal experience you will want to add <code>@types/react</code> to your devDependencies.</p><div class="language-bash" data-ext="sh"><pre class="language-bash"><code><span class="token function">npm</span> <span class="token function">install</span> <span class="token parameter variable">-D</span> @types/react
+<span class="token function">yarn</span> <span class="token function">add</span> <span class="token parameter variable">-D</span> @types/react
+<span class="token function">pnpm</span> <span class="token function">add</span> <span class="token parameter variable">-D</span> @types/react
+</code></pre></div><h2 id="getting-the-right-jsx-types" tabindex="-1"><a class="header-anchor" href="#getting-the-right-jsx-types" aria-hidden="true">#</a> Getting the right jsx types</h2><p>In order to get a complete typescript experience you&#39;ll want to add these two rules to your <code>tsconfig.json</code></p><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+    <span class="token property">&quot;compilerOptions&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+        ...
+        <span class="token property">&quot;jsx&quot;</span><span class="token operator">:</span> <span class="token string">&quot;react-jsx&quot;</span><span class="token punctuation">,</span>
+        <span class="token property">&quot;jsxImportSource&quot;</span><span class="token operator">:</span> <span class="token string">&quot;reaccord&quot;</span>
+        ...
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7),p=[o];function i(c,r){return n(),a("div",null,p)}const d=s(t,[["render",i],["__file","typescript.html.vue"]]);export{d as default};
